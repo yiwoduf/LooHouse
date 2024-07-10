022f0b84424a8dab055699f6fda56bec244036e7
leeable <yiwoduf@gmail.com>
2024-07-09 23:33:15 -0500

    For PythonAnyWhere

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from flask import Flask
 from api import create_app
-
 
 app = create_app()
 
+def lambda_handler(event, context):
+    return app(event, context)
 
 if __name__ == '__main__':
     app.run(port=5328)
